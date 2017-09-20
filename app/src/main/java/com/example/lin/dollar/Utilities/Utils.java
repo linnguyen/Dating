e0f8d080914331a3782b8871a15ad7401b3f891d
linnguyen <nguyenvanlin01041994@gmail.com>
2017-09-20 07:28:45 +0700

    Reflect to view for login

---
+++ app/src/main/java/com/example/lin/dollar/Utilities/Utils.java
@@ -0,0 +1,14 @@
+package com.example.lin.dollar.Utilities;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by lin on 20/09/2017.
+ */
+
+public class Utils {
+    public static void showToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+}
--- app/src/main/java/com/example/lin/dollar/activity/Login/LoginActivity.java
+++ app/src/main/java/com/example/lin/dollar/activity/Login/LoginActivity.java
@@ -1,15 +1,45 @@
 package com.example.lin.dollar.activity.Login;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.bumptech.glide.util.Util;
 import com.example.lin.dollar.R;
+import com.example.lin.dollar.Utilities.Utils;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    private Context context;
+
+    private EditText edtEmail;
+    private EditText edtPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        context = getApplicationContext();
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        edtPassword = (EditText) findViewById(R.id.edtPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Utils.showToast(context, "login");
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnLogin:
+                break;
+        }
     }
 }
